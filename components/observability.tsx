0b960879290c298d71ffb04e38cdd4fd8026827e
Howard Yoo <32691630+howardyoo@users.noreply.github.com>
2025-03-03 12:42:06 -0600

    Update observability.tsx
    removed the api key exposed.
    note: the exposed api key has been disabled and removed.

--- components/observability.tsx
+++ components/observability.tsx
@@ -20,7 +20,7 @@ export default function Observability(){
         const sdk = new HoneycombWebSDK({
         // endpoint: "https://api.eu1.honeycomb.io/v1/traces", // Send to EU instance of Honeycomb. Defaults to sending to US instance.
         debug: true, // Set to false for production environment.
-        apiKey: 'hcaik_01jkrp107n9vj806wjejpa1k5xfggjmq3d1krnr9e0wxn09bpmd67b31x4', // Replace with your Honeycomb Ingest API Key.
+        apiKey: '<YOUR_API_KEY_HERE>', // Replace with your Honeycomb Ingest API Key.
         serviceName: 'nextjs-web', // Replace with your application name. Honeycomb uses this string to find your dataset when we receive your data. When no matching dataset exists, we create a new one with this name if your API Key has the appropriate permissions.
         instrumentations: [getWebAutoInstrumentations({
             // Loads custom configuration for xml-http-request instrumentation.
